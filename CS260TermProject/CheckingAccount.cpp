b994381137fefe2fb38f393d380d705471b0c8d0
frostbyte-png <spiritofwar237@gmail.com>
2026-04-08 10:42:52 -0400

    CheckingAccount.cpp

--- CS260TermProject/CheckingAccount.cpp
+++ CS260TermProject/CheckingAccount.cpp
@@ -1,1 +1,26 @@
+#include "CheckingAccount.h"
+#include <iomanip>
+#include <iostream>
+using namespace std;
 
+void CheckingAccount::setOverDraftLimit(double _overDraftLimit) {
+	if (_overDraftLimit >= 0)
+		overDraftLimit = _overDraftLimit;
+	else
+		overDraftLimit = 0;
+}
+
+double CheckingAccount::getOverDraftLimit() const {
+	return overDraftLimit;
+}
+
+CheckingAccount::CheckingAccount() : Account() {
+	overDraftLimit = 0;
+}
+
+CheckingAccount::CheckingAccount(int _id, double _balance, Customer* _customerAccount, double _overDraftLimit) : Account(_id, _balance, _customerAccount) {
+	if (_overDraftLimit >= 0)
+		overDraftLimit = _overDraftLimit;
+	else
+		overDraftLimit = 0;
+}
