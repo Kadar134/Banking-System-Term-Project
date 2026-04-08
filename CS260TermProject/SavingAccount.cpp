7bdf90d80d770ed123e22a56e632464929c7611a
frostbyte-png <spiritofwar237@gmail.com>
2026-04-08 13:18:47 -0400

    Update SavingAccount.cpp

--- CS260TermProject/SavingAccount.cpp
+++ CS260TermProject/SavingAccount.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include "SavingAccount.h"
+using namespace std;
 
+void SavingAccount::setInterestRate(double _interestRate) {
+	if (_interestRate > 0)
+		interestRate = _interestRate;
+	else
+		interestRate = 0;
+}
+
+void SavingAccount::setAll(int _id, double _balance, Customer* _customerAccount, double _interestRate) {
+	Account::setAll(_id, _balance, _customerAccount);
+
+	if (_interestRate > 0)
+		interestRate = _interestRate;
+	else
+		interestRate = 0;
+}
+
+double SavingAccount::getInterestRate() const {
+	return interestRate;
+}
+
+// fix me later
+void SavingAccount::payInterest() {
+	double balance;
+	balance = getBalance();
+	balance = balance + (balance * interestRate);
+}
+
+void SavingAccount::transfer(double amount, SavingAccount destinationAccount) {
+	// put something here later
+}
+
+SavingAccount::SavingAccount() : Account() {
+	interestRate = 0;
+}
+
+SavingAccount::SavingAccount(int _id, double _balance, Customer* _customerAccount, double _interestRate) : Account(_id, _balance, _customerAccount) {
+	if (_interestRate > 0)
+		interestRate = _interestRate;
+	else
+		interestRate = 0;
+}
+
+// Note: there are errors and incomplete code. I will work on this later.
