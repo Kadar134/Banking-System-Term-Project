1a845cc393cfc86ad0227fff94ab82323756b1c3
frostbyte-png <spiritofwar237@gmail.com>
2026-04-08 10:44:00 -0400

    CheckingAccount.h

--- CS260TermProject/CheckingAccount.h
+++ CS260TermProject/CheckingAccount.h
@@ -1,1 +1,20 @@
+#pragma once
+#ifndef _CHECKING_ACCOUNT_
+#define _CHECKING_ACCOUNT_
+#include "Account.h"
+using namespace std;
 
+class CheckingAccount : public Account {
+	double overDraftLimit;
+
+public:
+	void setOverDraftLimit(double _overDraftLimit);
+	double getOverDraftLimit() const;
+
+	CheckingAccount() : Account();
+	CheckingAccount(int _id, double _balance, Customer* _customerAccount, double _overDraftLimit) : Account(_id, _balance, _customerAccount);
+};
+
+
+
+#endif // !_CHECKING_ACCOUNT_
