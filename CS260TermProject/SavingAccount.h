2d4e8c996804fb35fedc7bed1ce94502e8cb4efc
frostbyte-png <spiritofwar237@gmail.com>
2026-04-08 13:21:56 -0400

    Update SavingAccount.h

--- CS260TermProject/SavingAccount.h
+++ CS260TermProject/SavingAccount.h
@@ -8,13 +8,16 @@ class SavingAccount : Account {
 	double interestRate;
 
 public:
+	// Constructors
 	SavingAccount();
 	SavingAccount(int _id, double _balance, Customer* _customerAccount, double _interestRate);
-	
+
+	// Setters and Getters
 	void setInterestRate(double _interestRate);
 	void setAll(int _id, double _balance, Customer* _customerAccount, double _interestRate);
 	double getInterestRate() const;
 
+	// Specs
 	void payInterest();
 	void transfer(double amount, SavingAccount destinationAccount);
 };
