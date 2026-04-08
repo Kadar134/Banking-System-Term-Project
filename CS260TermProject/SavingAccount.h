280087889f068bef1a78e16fbd41f1494975c267
frostbyte-png <spiritofwar237@gmail.com>
2026-04-08 13:19:19 -0400

    Update SavingAccount.h

--- CS260TermProject/SavingAccount.h
+++ CS260TermProject/SavingAccount.h
@@ -1,3 +1,24 @@
-MY FILE THIS IS MY FILE RAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+#pragma once
+#ifndef _SAVINGS_ACCOUNT_
+#define _SAVINGS_ACCOUNT_
+#include "Account.h"
+using namespace std;
 
-I WILL MAKE THE HEADER AND THE CPP TONGIHT EWSIGDJFB EAWRLKJHSGKAWFBHEWKRF STAEG
+class SavingAccount : Account {
+	double interestRate;
+
+public:
+	void setInterestRate(double _interestRate);
+	void setAll(int _id, double _balance, Customer* _customerAccount, double _interestRate);
+	double getInterestRate() const;
+
+	void payInterest();
+	void transfer(double amount, SavingAccount destinationAccount);
+
+	SavingAccount();
+	SavingAccount(int _id, double _balance, Customer* _customerAccount, double _interestRate);
+};
+
+#endif // !_SAVINGS_ACCOUNT_
+
+// Header file is complete.
