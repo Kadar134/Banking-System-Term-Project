16cc51549dcafeeb965b51d71624a21ef4d8b5e5
frostbyte-png <spiritofwar237@gmail.com>
2026-04-10 16:17:23 -0400

    Update Account.cpp

--- CS260TermProject/Account.cpp
+++ CS260TermProject/Account.cpp
@@ -1,165 +1,45 @@
 #include <iostream>
-#include <iomanip>
-#include "Customer.h"
-#include "Account.h"
+#include "SavingAccount.h"
 using namespace std;
 
-void Account::setid(int _id)
-{
-	if (_id < 0)
-	{
-		id = 0;
-	}
+// Constructors
+SavingAccount::SavingAccount() : Account() {
+	interestRate = 0;
+}
+
+SavingAccount::SavingAccount(int _id, double _balance, Customer* _customerAccount, double _interestRate) : Account(_id, _balance, _customerAccount) {
+	if (_interestRate > 0)
+		interestRate = _interestRate;
 	else
-	{
-		id = _id;
-	}
+		interestRate = 0;
 }
 
-void Account::setBalance(double _balance)
-{
-	if (_balance < 0)
-	{
-		balance = 0;
-	}
+// Setters
+void SavingAccount::setInterestRate(double _interestRate) {
+	if (_interestRate > 0)
+		interestRate = _interestRate;
 	else
-	{
-		balance = _balance;
-	}
+		interestRate = 0;
 }
 
-void Account::setWithDrawCounter(int _withdrawCounter)
-{
-	withdrawalCounter = _withdrawCounter;
+void SavingAccount::setAll(int _id, double _balance, Customer* _customerAccount, double _interestRate) {
+	Account::setAll(_id, _balance, _customerAccount);
 
-}
-
-void Account::setDepositCounter(int _depositCounter)
-{
-	depositsCounter = _depositCounter;
-}
-
-void Account::setAccount(Customer* account) {
-	accountCustomer = account;
-}
-
-int Account::getid() const {
-	return id;
-}
-
-double Account::getBalance() const {
-	return balance;
-}
-
-int Account::getWithdrawCounter() const {
-	return withdrawalCounter;
-}
-
-int Account::getDepositCounter() const {
-	return depositsCounter;
-}
-
-Customer* Account::getCustomer() const {
-	return accountCustomer;
-}
-
-//Default
-Account::Account() {
-	id = 0;
-	balance = 0;
-	withdrawalCounter = 0;
-	depositsCounter = 0;
-	accountCustomer = nullptr;
-}
-
-//Parameterized
-Account::Account(int _id, double _balance, Customer* _customerAccount)
-{
-	if (_id < 0)
-	{
-		id = 0;
-	}
+	if (_interestRate > 0)
+		interestRate = _interestRate;
 	else
-	{
-		id = _id;
-	}
-
-	if (_balance < 0)
-	{
-		balance = 0;
-	}
-	else
-	{
-		balance = _balance;
-	}
-
-	//These are set to 0 because they're counters
-	withdrawalCounter = 0;
-	depositsCounter = 0;
-
-	accountCustomer = _customerAccount;
+		interestRate = 0;
 }
 
-
-void Account::setAll(int _id, double _balance, Customer* _accountCustomer)
-{
-	if (_id < 0)
-	{
-		id = 0;
-	}
-	else
-	{
-		id = _id;
-	}
-	balance = _balance;
-	accountCustomer = _accountCustomer;
-	//No reason to set the counters to 0 because you're not manually setting those to anything
+double SavingAccount::getInterestRate() const {
+	return interestRate;
 }
 
-
-void Account::depositMoney(double amount) {
-	if (amount < 0)
-	{
-		balance += 0;
-	}
-	else
-	{
-		balance += amount;
-		depositsCounter++; //Updates the deposit counter
-	}
+// fix me later
+void SavingAccount::payInterest() {
+	balance = balance + (balance * interestRate);
 }
 
-void Account::withdrawMoney(double amount) {
-	if (amount > 0) {
-		if (amount <= balance)
-		{
-			balance -= amount;
-			withdrawalCounter++; //Updates the withdraw counter
-		}
-		else
-			cout << "Insufficient funds." << endl;
-	}
-	else
-		balance -= 0;
-}
-
-
-void Account::printInfo() {
-	if (accountCustomer != nullptr) {
-		cout << "Retrieved Banking Information for: ";
-		cout << accountCustomer->getFname() << " " << accountCustomer->getLname() << endl;
-
-		accountCustomer->printInfo();
-
-		cout << "Bank ID: " << id << endl;
-
-		cout << "Current Balance: $";
-		cout << fixed << showpoint << setprecision(2) << balance << endl;
-
-		cout << "Withdraws Made: " << withdrawalCounter << endl;
-		cout << "Deposits Made: " << depositsCounter << endl;
-	}
-
-	else
-		cout << "No customer assigned." << endl;
+void SavingAccount::transfer(double amount, SavingAccount destinationAccount) {
+	// THIS IS NOT FINISHED
 }
