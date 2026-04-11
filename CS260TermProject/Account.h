da97d47ad143e8f75d80d2427f9671d499d63bbc
Kadar <kadar.rahming@gmail.com>
2026-04-11 16:18:30 -0400

    Update Account.h

--- CS260TermProject/Account.h
+++ CS260TermProject/Account.h
@@ -7,38 +7,32 @@ using namespace std;
 //Account class
 class Account {
 	int id;
-	int withdrawalCounter = 0;
-	int depositsCounter = 0;
 	Customer* accountCustomer;
 protected:
 	double balance;
+	int withdrawalCounter = 0, depositsCounter = 0;
 public:
-	// Constructors
 	Account();
 	Account(int _id, double _balance, Customer* _accountCustomer);
 
 	//Setters
 	void setid(int _id);
+	void setAccount(Customer* _accountCustomer);
 	void setBalance(double _balance);
-	void setAccount(Customer* account);
-	void setWithDrawCounter(int _withdrawCounter);
-	void setDepositCounter(int _depositCounter);
-	void setAll(int _id, double _balance, Customer* account);
 
 	//Getters
 	int getid() const;
-	double getBalance() const;
 	Customer* getCustomer() const;
+	double getBalance() const;
 	int getWithdrawCounter() const;
 	int getDepositCounter() const;
 
-	// Print
-	void printInfo();
+	void setAll(int _id, double _balance, Customer* _accountCustomer);
 
-	// Deposit & Withdraw
 	void depositMoney(double amount);
 	void withdrawMoney(double amount);
 
+	void printInfo();
 };
 
 #endif // !ACCOUNT_H
