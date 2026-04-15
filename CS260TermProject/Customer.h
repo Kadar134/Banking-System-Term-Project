5086740b89cb67b1f90f6c05178de53c028d0482
frostbyte-png <spiritofwar237@gmail.com>
2026-04-15 09:39:59 -0400

    Update Customer.h

--- CS260TermProject/Customer.h
+++ CS260TermProject/Customer.h
@@ -6,30 +6,28 @@ using namespace std;
 
 //Customer class
 class Customer {
-	string fname, lname, address;
-	int phone;
-	string email;
+	string fname, lname, address, phone, email;
 public:
 	//Constructors
 	Customer();
-	Customer(string _fname, string _lname, string _address, int _phone, string _email);
+	Customer(string _fname, string _lname, string _address, string _phone, string _email);
 
 	//Setters
 	void setFname(string _fname);
 	void setLname(string _fname);
 	void setAddress(string _fname);
-	void setPhone(int _fname);
+	void setPhone(string _fname);
 	void setEmail(string _fname);
 
 	//Getters
 	string getFname() const;
 	string getLname() const;
 	string getAddress() const;
-	int getPhone() const;
+	string getPhone() const;
 	string getEmail() const;
 
 	//SetAll
-	void setAll(string _fname, string _lname, string _address, int _phone, string _email);
+	void setAll(string _fname, string _lname, string _address, string _phone, string _email);
 
 	//Print
 	void printInfo();
